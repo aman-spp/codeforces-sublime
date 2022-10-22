d3512d84e9ee7a997fbd7fd5e9a8b2b9466c3442
Aman Babu <72257053+aman-spp@users.noreply.github.com>
2022-10-22 15:44:40 +0530

    initial commit

---
+++ aman.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1e9+7
+void solve(){
+    
+}
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ll test=1;
+    //cin>>test;
+    while(test--){
+        solve();
+    }
+    return 0;
+}
